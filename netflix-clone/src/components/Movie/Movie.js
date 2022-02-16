37cbdebf0fe5076452cde156ff355d8068d42837
SalimHass <salim.hassouneh@gmail.com>
2022-02-16 17:36:36 +0200

    task18

--- netflix-clone/src/components/Movie/Movie.js
+++ netflix-clone/src/components/Movie/Movie.js
@@ -1,5 +1,6 @@
 import ModalMovie from "../ModalMovie/ModalMovie";
 import "./Movie.css"
+import { Button } from "react-bootstrap";
 import React, { useState, useEffect } from 'react';
 const Movie = (props) => {
 
@@ -10,7 +11,7 @@ const Movie = (props) => {
             <h3>{props.movie.title}</h3> <br />
             <img src={`https://image.tmdb.org/t/p/w300${props.movie.poster_path}`} alt={props.movie.title} />
             <br />
-            <button type="button" onClick={()=>{setShowModal(true)}}>Add to Favorite</button>
+            <Button onClick={()=>{setShowModal(true)}}>Add to Favorite</Button>
         </div>
     </li>)
 }
